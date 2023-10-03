b721af47201f27b73c70ffd782126d72a5fcd196
Patryk Nowak <patryk.nowak@taskscape.com>
2023-10-03 11:21:37 +0200

    Final implementation of conditional list rendering

--- ReactProject/src/components/ListGroup.tsx
+++ ReactProject/src/components/ListGroup.tsx
@@ -6,7 +6,7 @@ function ListGroup() {
   return (
     <>
       <h1>List Group</h1>
-      {items.length === 0 ? <p>No items found</p> : null}
+      {items.length === 0 && <p>No items found</p>}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item} className="list-group-item">
